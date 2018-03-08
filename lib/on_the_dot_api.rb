696b311fc3347cfd95e9e8494fdd3a1c2c7ac605
Gediminas <gediminas.zubovicius@gmail.com>
2018-03-08 14:25:43 +0000

    cancel booking operation + refactoring operations parameters

--- lib/on_the_dot_api.rb
+++ lib/on_the_dot_api.rb
@@ -9,6 +9,7 @@ require 'on_the_dot_api/errors/response_error'
 require 'on_the_dot_api/operation'
 require 'on_the_dot_api/operations/get_available_timeslots'
 require 'on_the_dot_api/operations/create_booking'
+require 'on_the_dot_api/operations/cancel_booking'
 
 module OnTheDotApi
 end
---
+++ lib/on_the_dot_api/operations/cancel_booking.rb
@@ -0,0 +1,13 @@
+module OnTheDotApi
+  module Operations
+    class CancelBooking < Operation
+      def http_method
+        :delete
+      end
+
+      def endpoint
+        "#{base_url}/v1.0/cancelbooking/#{store_id}/#{options[:order_number]}"
+      end
+    end
+  end
+end
